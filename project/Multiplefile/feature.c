1b96b5310026f59e3cfa0488d126b0a7b133575b
vempatikalyan <94208305+vempatikalyan@users.noreply.github.com>
2021-11-23 23:47:01 +0530

    Add files via upload

---
+++ project/Multiplefile/feature.c
@@ -0,0 +1,29 @@
+#include "feature.h"
+/*
+    Avoid printf and scanf functions
+*/
+int addition_funtion(int num1, int num2)
+{
+    return num1 + num2;
+}
+
+int subtract(int num1, int num2)
+{
+    return num1 - num2;
+}
+
+int sum ;
+int* divide(int numerator, int denominator )
+{
+    if(0 == denominator)
+    {
+
+    }
+    int* var;
+
+    return var;
+}
+
+/*
+    Unit testing
+*/
---
+++ project/Multiplefile/feature.h
@@ -0,0 +1,42 @@
+/**
+ * @file feature.h
+ * @author Bharath.G ()
+ * @brief Prototype for feature functions
+ * @version 0.1
+ * @date 2021-11-17
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef FEATURE_H_  /* Header Gaurd*/
+#define FEATURE_H_
+
+/**
+ * @brief Function for adding two integer numbers
+ * 
+ * @param num1 Number1
+ * @param num2 Number2
+ * @return int Result of Addition
+ */
+int addition_funtion(int num1, int num2);
+
+/**
+ * @brief Function for adding two integer numbers
+ * 
+ * @param num1 Number1
+ * @param num2 Number1
+ * @return int Result of Subtraction
+ */
+int subtract(int num1, int num2);
+
+#endif
+
+
+/*
+Building or Compilation
+    1. Preprocessing
+    2. Compilation
+    3. Assembling
+    4. Linking
+*/
